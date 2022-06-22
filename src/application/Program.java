3a7f0ccd029c846434e31bf206f96ccd76df98c4
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-06-22 20:51:45 -0300

    Department entity class created

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entitites.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department d1 = new Department(1,"Books");
+
+        System.out.println(d1);
+
+    }
+
+}
