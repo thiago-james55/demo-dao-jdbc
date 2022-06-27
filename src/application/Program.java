44d6f5d4a1a305e60005c85f246dfa1cd72baac9
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-06-27 10:48:46 -0300

    Created SellerDaoJDBC e respective Factory!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entitites.Department;
 import model.entitites.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
         Department d1 = new Department(1,"Books");
 
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(), 3000.0, d1);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller.toString());
 
